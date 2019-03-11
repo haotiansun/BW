c98d613e19599c54da08183b8e2c2eef6be3af2d
jzy95310 <45862046+jzy95310@users.noreply.github.com>
2019-03-11 10:15:52 -0700

    Add files via upload

--- bunnyworld/Shape.java
+++ bunnyworld/Shape.java
@@ -16,19 +16,34 @@ import java.util.Arrays;
 
 public class Shape {
 
-	private float x, y, width, height, x_backup, y_backup, width_backup, height_backup;     // Coordinates of top-left and bottom-right corner of the shape
+	private float x, y, width, height;         // x and y are the coordinates of the center of the shape
+	private ArrayList<Float> x_backup = new ArrayList<Float>();
+	private ArrayList<Float> y_backup = new ArrayList<Float>();
+	private ArrayList<Float> width_backup = new ArrayList<Float>();
+	private ArrayList<Float> height_backup = new ArrayList<Float>();
 
-	private int type, type_backup;                      // determine if the shape is rectangle, image or text
-	private String name, name_backup;
-	private String imageName, imageName_backup;            // Name of the associated image
-	private Script script, script_backup;
-	private String text, text_backup;
-	private float textSize, textSize_backup;
-	private String pageName, pageName_backup;    // List of pages which contain the corresponding shape
+	private int type;                      // determine if the shape is rectangle, image or text
+	private ArrayList<Integer> type_backup = new ArrayList<Integer>();
+	private String name;
+	private ArrayList<String> name_backup = new ArrayList<String>();
+	private String imageName;            // Name of the associated image
+	private ArrayList<String> imageName_backup = new ArrayList<String>();
+	private Script script;
+	private ArrayList<Script> script_backup = new ArrayList<Script>();
+	private String text;
+	private ArrayList<String> text_backup = new ArrayList<String>();
+	private float textSize;
+	private ArrayList<Float> textSize_backup = new ArrayList<Float>();
+	private String pageName;    // List of pages which contain the corresponding shape
+	private ArrayList<String> pageName_backup = new ArrayList<String>();
 	
-	private boolean movable, movable_backup;             // True if the shape is movable
-	private boolean visible, visible_backup;             // True if the shape is visible
-	private boolean useImageBounds, useImageBounds_backup;              // True if the user choose to use default size of the image
+	private boolean movable;             // True if the shape is movable
+	private ArrayList<Boolean> movable_backup = new ArrayList<Boolean>();
+	private boolean visible;             // True if the shape is visible
+	private ArrayList<Boolean> visible_backup = new ArrayList<Boolean>();
+	private boolean useImageBounds;              // True if the user choose to use default size of the image
+	private ArrayList<Boolean> useImageBounds_backup = new ArrayList<Boolean>();
+	private String undoType;
 	
 	private static int shapeNum = 0;         // Incremented when a new shape is created
 	private static final int RECTANGLE = 1;
@@ -37,6 +52,7 @@ public class Shape {
 	private static final float DEFAULT_TEXTSIZE = 50.0f;
 	private static final ArrayList<String> imageList = new ArrayList<String>(Arrays.asList("carrot", "carrot2", "death", "duck", "fire", "mystic"));
 
+	// Trigger and Actions
 	private static final String ONCLICK = "on click";
 	private static final String ONENTER = "on enter";
 	private static final String ONDROP = "on drop";
@@ -45,6 +61,7 @@ public class Shape {
 	private static final String HIDE = "hide";
 	private static final String SHOW = "show";
 
+	// Name of Sounds
 	private static final String CARROT = "carrot-eating";
 	private static final String EVIL = "evil-laugh";
 	private static final String FIRE = "fire-sound";
@@ -53,17 +70,32 @@ public class Shape {
 	private static final String MUNCHING = "munching";
 	private static final String WOOF = "woof";
 
+	// Name of undo types
+	private static final String UNDO_X = "undo_x";
+	private static final String UNDO_Y = "undo_y";
+	private static final String UNDO_WIDTH = "undo_width";
+	private static final String UNDO_HEIGHT = "undo_height";
+	private static final String UNDO_NAME = "undo_name";
+	private static final String UNDO_IMAGENAME = "undo_imageName";
+	private static final String UNDO_SCRIPT = "undo_script";
+	private static final String UNDO_TEXT  = "undo_text";
+	private static final String UNDO_TEXTSIZE = "undo_textSize";
+	private static final String UNDO_PAGENAME = "undo_pageName";
+	private static final String UNDO_MOVABLE = "undo_movable";
+	private static final String UNDO_VISIBLE = "undo_visible";
+	private static final String UNDO_USEIMAGEBOUNDS = "undo_useImageBounds";
+
 	private Paint grayFillPaint;
 	private Paint blueOutlinePaint;
 	private Paint textPaint;
-	public static Context context;
+	private static Context context;
 	
 	public Shape(float x, float y, float width, float height) {
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		geometryBackup();
+		xBackup(); yBackup(); widthBackup(); heightBackup();
 
 		type = RECTANGLE; typeBackup();
 		shapeNum++;
@@ -73,6 +105,7 @@ public class Shape {
 		text = ""; textBackup();
 		pageName = ""; pageNameBackup();
 		textSize = DEFAULT_TEXTSIZE;
+		undoType = "";
 		
 		movable = false; movableBackup();
 		visible = true; visibleBackup();
@@ -101,7 +134,7 @@ public class Shape {
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		geometryBackup();
+		xBackup(); yBackup(); widthBackup(); heightBackup();
 
 		shapeNum++;
 		// Check if the shape name has already been used. If yes, use default shape name
@@ -164,6 +197,7 @@ public class Shape {
 		textBackup();
 
 		textSize = tSize;
+		undoType = "";
 
 		grayFillPaint = new Paint();
 		grayFillPaint.setColor(Color.rgb(211, 211, 211));
@@ -177,24 +211,76 @@ public class Shape {
 
 	}
 
+	// Copy Constructor, use this constructor to copy a shape
+	// Pass in a Shape object you want to copy from
+	// The name of the shape will be the name of the copied shape plus "_copy"
+	public Shape(Shape shape) {
+		this.x = shape.x; xBackup();
+		this.y = shape.y; yBackup();
+		this.width = shape.width; widthBackup();
+		this.height = shape.height; heightBackup();
+
+		shapeNum++;
+		name = shape.name + "_copy"; nameBackup();
+		type = shape.type; typeBackup();
+		imageName = shape.imageName; imageNameBackup();
+		script = shape.script; scriptBackup();
+		text = shape.text; textBackup();
+		textSize = shape.textSize; textSizeBackup();
+		pageName = shape.pageName; pageNameBackup();
+		movable = shape.movable; movableBackup();
+		visible = shape.visible; visibleBackup();
+		useImageBounds = shape.useImageBounds; useImageBoundsBackup();
+		undoType = "";
+
+		grayFillPaint = new Paint();
+		grayFillPaint.setColor(Color.rgb(211, 211, 211));
+		blueOutlinePaint = new Paint();
+		blueOutlinePaint.setColor(Color.BLUE);
+		blueOutlinePaint.setStyle(Paint.Style.STROKE);
+		blueOutlinePaint.setStrokeWidth(20.0f);
+
+		textPaint = new Paint();
+		textPaint.setColor(Color.BLACK);
+	}
+
 	//-------------------------------Set shape parameters--------------------------------------------------
 	// Here setX and setY is only used for game editing, in game playing we use move()
 	public void setX(float x) {
+		xBackup();
+		undoType = UNDO_X;
 		this.x = x;
 	}
 
 	public void setY(float y) {
+		yBackup();
+		undoType = UNDO_Y;
 		this.y = y;
+	}
+
+	public void setWidth(float width) {
+		widthBackup();
+		undoType = UNDO_WIDTH;
+		this.width = width;
+	}
+
+	public void setHeight(float height) {
+		heightBackup();
+		undoType = UNDO_HEIGHT;
+		this.height = height;
 	}
 
 	public void setName(String str) {
 		nameBackup();
+		undoType = UNDO_NAME;
 		this.name = str;
 	}
 	
 	public void setImageName(String imageName) {
 		// If there's no such image, shape remains to be a rectangle
 		imageNameBackup();
+		typeBackup();
+		undoType = UNDO_IMAGENAME;
 		if (imageList.contains(imageName)) {
 			this.imageName = imageName;
 			// If shape contains text, text takes precedence
@@ -209,22 +295,27 @@ public class Shape {
 	
 	public void setScript(String str) {
 		scriptBackup();
+		undoType = UNDO_SCRIPT;
 		this.script = new Script(str);
 	}
 	
 	public void setText(String text) {
 		textBackup();
+		typeBackup();
+		undoType = UNDO_TEXT;
 		this.text = text;
 		type = TEXT;
 	}
 
 	public void setTextSize(float textSize) {
 		textSizeBackup();
+		undoType = UNDO_TEXTSIZE;
 		this.textSize = textSize;
 	}
 	
 	public void setMovable(boolean movable) {
 		movableBackup();
+		undoType = UNDO_MOVABLE;
 		this.movable = movable;
 		if (!visible) {
 			this.movable = false;
@@ -233,6 +324,7 @@ public class Shape {
 	
 	public void setVisible(boolean visible) {
 		visibleBackup();
+		undoType = UNDO_VISIBLE;
 		this.visible = visible;
 		if (!visible) {
 			this.movable = false;
@@ -241,16 +333,17 @@ public class Shape {
 	
 	public void setUseImageBounds(boolean bool) {
 		useImageBoundsBackup();
+		undoType = UNDO_USEIMAGEBOUNDS;
 		this.useImageBounds = bool;
 	}
 
 	public void setPageName(String pageName) {
 		pageNameBackup();
+		undoType = UNDO_PAGENAME;
 		this.pageName = pageName;
 	}
 	
 	//-------------------------------Get shape parameters--------------------------------------------------
-	
 	public float getWidth() {
 		return width;
 	}
@@ -290,8 +383,6 @@ public class Shape {
 	public String getText() {
 		return text;
 	}
-
-	public float getTextSize() { return textSize; }
 
 	//-------------------Used for data saving-------------------------
 	public int getMov() {
@@ -371,26 +462,32 @@ public class Shape {
 							carrotDrawable = (BitmapDrawable) context.getResources().getDrawable(R.drawable.carrot);
 							Bitmap carrotMap = carrotDrawable.getBitmap();
 							canvas.drawBitmap(carrotMap, x - carrotMap.getWidth()/2.0f, y - carrotMap.getHeight()/2.0f, null);
+							width = (float)carrotMap.getWidth(); height = (float)carrotMap.getHeight();
 						} else if (imageName.equals("carrot2")) {
 							carrot2Drawable = (BitmapDrawable) context.getResources().getDrawable(R.drawable.carrot2);
 							Bitmap carrot2Map = carrot2Drawable.getBitmap();
 							canvas.drawBitmap(carrot2Map, x - carrot2Map.getWidth()/2.0f, y - carrot2Map.getHeight()/2.0f, null);
+							width = (float)carrot2Map.getWidth(); height = (float)carrot2Map.getHeight();
 						} else if (imageName.equals("death")) {
 							deathDrawable = (BitmapDrawable) context.getResources().getDrawable(R.drawable.death);
 							Bitmap deathMap = deathDrawable.getBitmap();
 							canvas.drawBitmap(deathMap, x - deathMap.getWidth()/2.0f, y - deathMap.getHeight()/2.0f, null);
+							width = (float)deathMap.getWidth(); height = (float)deathMap.getHeight();
 						} else if (imageName.equals("duck")) {
 							duckDrawable = (BitmapDrawable) context.getResources().getDrawable(R.drawable.duck);
 							Bitmap duckMap = duckDrawable.getBitmap();
 							canvas.drawBitmap(duckMap, x - duckMap.getWidth()/2.0f, y - duckMap.getHeight()/2.0f, null);
+							width = (float)duckMap.getWidth(); height = (float)duckMap.getHeight();
 						} else if (imageName.equals("fire")) {
 							fireDrawable = (BitmapDrawable) context.getResources().getDrawable(R.drawable.fire);
 							Bitmap fireMap = fireDrawable.getBitmap();
 							canvas.drawBitmap(fireMap, x - fireMap.getWidth()/2.0f, y - fireMap.getHeight()/2.0f, null);
+							width = (float)fireMap.getWidth(); height = (float)fireMap.getHeight();
 						} else if (imageName.equals("mystic")) {
 							mysticDrawable = (BitmapDrawable) context.getResources().getDrawable(R.drawable.mystic);
 							Bitmap mysticMap = mysticDrawable.getBitmap();
 							canvas.drawBitmap(mysticMap, x - mysticMap.getWidth()/2.0f, y - mysticMap.getHeight()/2.0f, null);
+							width = (float)mysticMap.getWidth(); height = (float)mysticMap.getHeight();
 						}
 					}
 				}
@@ -406,7 +503,7 @@ public class Shape {
 
 	// Move to a certain location (x, y)
 	public boolean move(float x, float y) {
-		geometryBackup();
+		xBackup(); yBackup();
 		if (movable && visible) {
 			this.x = x;
 			this.y = y;
@@ -559,48 +656,60 @@ public class Shape {
 	}
 
 	//-------------------------------Backup for Undo Support--------------------------------------------------
-	public void geometryBackup() {
-		x_backup = x; y_backup = y; width_backup = width; height_backup = height;
+	public void xBackup() {
+		x_backup.add(x);
+	}
+
+	public void yBackup() {
+		y_backup.add(y);
+	}
+
+	public void widthBackup() {
+		width_backup.add(width);
+	}
+
+	public void heightBackup() {
+		height_backup.add(height);
 	}
 
 	public void typeBackup() {
-		type_backup = type;
+		type_backup.add(type);
 	}
 
 	public void nameBackup() {
-		name_backup = name;
+		name_backup.add(name);
 	}
 
 	public void imageNameBackup() {
-		imageName_backup = imageName;
+		imageName_backup.add(imageName);
 	}
 
 	public void textBackup() {
-		text_backup = text;
+		text_backup.add(text);
 	}
 
 	public void textSizeBackup() {
-		textSize_backup = textSize;
+		textSize_backup.add(textSize);
 	}
 
 	public void scriptBackup() {
-		script_backup = new Script(script);
+		script_backup.add(script);
 	}
 
 	public void pageNameBackup() {
-		pageName_backup = pageName;
+		pageName_backup.add(pageName);
 	}
 
 	public void movableBackup() {
-		movable_backup = movable;
+		movable_backup.add(movable);
 	}
 
 	public void visibleBackup() {
-		visible_backup = visible;
+		visible_backup.add(visible);
 	}
 
 	public void useImageBoundsBackup() {
-		useImageBounds_backup = useImageBounds;
+		useImageBounds_backup.add(useImageBounds);
 	}
 
 	public void undo() {
